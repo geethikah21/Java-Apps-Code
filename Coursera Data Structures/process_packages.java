b518af5604b8943565affd4b1439e97624dcd990
geethikah21 <geethikah21@gmail.com>
2019-09-29 11:07:15 -0500

    Added comments

--- Coursera Data Structures/process_packages.java
+++ Coursera Data Structures/process_packages.java
@@ -30,14 +30,35 @@ class Buffer {
         this.finish_time_ = new ArrayList<Integer>();
     }
 
+    /* this program simulates the processing of network packets as they come and leave.
+        the inputs of the program include the size of the network buffer(the number of
+        packets that can be processed at once) and the arrival/processing time
+        of each incoming packet. If the network buffer is full when a packet arrives,
+        that packet is dropped and isn't processed (a -1 is printed in the output). For a
+        packet that comes when there is space in the buffer, the arrival time
+        is printed
+     */
     public Response Process(Request request) {
         // write your code here
+        //calculate the time that the incoming packet is busy until
         int requestBusyUntil = request.arrival_time + request.process_time;
 
+        //remove all the packets that will be finished processing when the current
+        //packet being processed arrives
         while(!finish_time_.isEmpty() && finish_time_.get(0) <= request.arrival_time) {
             finish_time_.remove(0);
         }
 
+        //if there are no packets being processed after removing all
+        //packets that are finished processing, add finish time to arraylist
+        //and then output the start time
+
+        //if there are packets still being processed, check to see if the buffer is
+        //full. If it is, then the current packet is dropped. Otherwise,
+        //output the finish time of the last packet (before adding the current one)
+        //to indicate the current packet's start time (called requestStartTime)
+        //and sum the process time of the current packet with requestStartTime to
+        //determine when the incoming packet (the passed parameter) will finish processing
         if(finish_time_.isEmpty()) {
             finish_time_.add(requestBusyUntil);
             return new Response(false, request.arrival_time);
