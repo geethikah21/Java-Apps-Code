b741a277a3fa53e38e46a8c59a92ff2b1d4f4426
geethikah21 <geethikah21@gmail.com>
2019-09-29 11:59:45 -0500

    Added comments

--- Coursera Data Structures/StackWithMax.java
+++ Coursera Data Structures/StackWithMax.java
@@ -22,33 +22,48 @@ public class StackWithMax {
         }
     }
 
+    /* this program extends the Stack interface by adding an additional method,
+        max(), to it. Max finds the maximum element in the stack at a given time.
+        The program reads input from the keyboard: the first line of input contains
+        the number of queries, n. The next n lines contain a query, which can be
+        either push, pop, or max. If the query is push, then a number will appear
+        next to it on that line.
+     */
     public void solve() throws IOException {
         FastScanner scanner = new FastScanner();
         int queries = scanner.nextInt();
-        Stack<Integer> stack = new Stack<Integer>();
-        ArrayList<Integer> max = new ArrayList<Integer>();
+        Stack<Integer> stack = new Stack<Integer>(); //stores the current stack
+        ArrayList<Integer> max = new ArrayList<Integer>(); //stores the max value(s)
         int maxNum = Integer.MIN_VALUE;
 
+        //processing each query
         for (int qi = 0; qi < queries; ++qi) {
             String operation = scanner.next();
             if ("push".equals(operation)) {
                 int value = scanner.nextInt();
+                //if the value being pushed onto the stack is greater than or equal
+                //to the current maximum, add it to the front of the max ArrayList
+
+                //if the operation is pop: if the number being popped is the same
+                //as the value at the front of the ArrayList max (the current max value),
+                //remove that value from the ArrayList as well. This makes the new
+                //max value the value that's now at the front of the ArrayList
+
+                //if the operation is max: print out the value at the front of the
+                //ArrayList
                 if(value >= maxNum) {
                     maxNum = value;
                     max.add(0, value);
                 }
                 stack.push(value);
-            } else if ("pop".equals(operation)) {
-                /*stack.peek().toString().equals(max.get(0).toString())*/
-                /*stack.peek() == max.get(0)*/
-                System.out.println(stack.peek() + " " + max.get(0));
+            }
+            else if ("pop".equals(operation)) {
                 if(stack.peek().toString().equals(max.get(0).toString())) {
                     max.remove(0);
                 }
-                System.out.println("popping " + stack.peek());
                 stack.pop();
-            } else if ("max".equals(operation)) {
-                //System.out.println(Collections.max(stack));
+            }
+            else if ("max".equals(operation)) {
                 System.out.println(max.get(0));
             }
         }
@@ -71,19 +86,3 @@ public class StackWithMax {
         new StackWithMax().solve();
     }
 }
-
-/*for(int i=0; i<numberOfQueriesBeforeMax; i++) {
-                    if(max_stack.isEmpty()) {
-                        max_stack.push(temp_stack.pop());
-                    }
-                    else {
-                        if(temp_stack.peek() > max_stack.peek()) {
-                            max_stack.push(temp_stack.pop());
-                        }
-                        else {
-                            temp_stack.pop();
-                        }
-                    }
-                }
-                System.out.println(max_stack.pop());
-                temp_stack = stack;*/
