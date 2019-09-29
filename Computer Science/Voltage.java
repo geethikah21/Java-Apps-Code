41ca22e4dc17b6dcb792f54b04ab3e5403c97fc5
geethikah21 <geethikah21@gmail.com>
2019-09-29 16:10:48 -0500

    Update Voltage.java

--- Computer Science/Voltage.java
+++ Computer Science/Voltage.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.io.*;
 
+/* A+ November 2015 #5 */
+
 public class Voltage {
     public static void main(String[] args) throws FileNotFoundException{
         Scanner scan = new Scanner(new File("voltage.dat"));
